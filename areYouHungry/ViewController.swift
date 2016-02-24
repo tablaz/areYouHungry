614c39529061839a22ae2ad63f17f446ad45df20
Ricardo Restrepo M <rrestrepo@seeketing.com>
2016-02-24 15:25:37 +0000

    Initial Commit

---
+++ areYouHungry/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  areYouHungry
+//
+//  Created by Ricardo on 24/02/2016.
+//  Copyright © 2016 Tablaz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
